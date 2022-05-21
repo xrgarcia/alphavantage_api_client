c09e3ecaf487b18f4fcc37db9df8701c59b23b30
Ray Garcia <ray@slashbin.us>
2022-05-21 11:37:02 -0500

    adding logic to generate version from git tag

--- __main__.py
+++ __main__.py
@@ -1,12 +1,12 @@
-from alphavantage_api_client.client import AlphavantageClient
+#from alphavantage_api_client.client import AlphavantageClient
 import json
-
+import subprocess
 def main():
     event = {
         "symbol": "TSLA"
     }
     result = {}
-    client = AlphavantageClient().with_api_key('[you key here]')
+    #client = AlphavantageClient().with_api_key('[you key here]')
     # result['overview'] = client.get_company_overview(event)
     # result['latest_stock_price'] = client.get_latest_stock_price(event)
     # result['stock_price'] = client.get_stock_price(event)
@@ -15,8 +15,11 @@ def main():
     # result['cash_flow'] = client.get_cash_flow(event)
     # result['latest_cash_flow'] = client.get_latest_cash_flow(event)
     # result['income_statement'] = client.get_income_statement_for_symbol(event)
-    result['latest_income_statement'] = client.get_latest_income_statement_for_symbol(event)
-    print(json.dumps(result))
+    #result['latest_income_statement'] = client.get_latest_income_statement_for_symbol(event)
+    #print(json.dumps(result))
+    alphavantage_api_client_version = subprocess.run(['git', 'describe', '--tags'],
+                                                     stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
+    print(alphavantage_api_client_version)
 if __name__ == "__main__":
     main()
 
--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
+import os.path
 import pathlib
+import subprocess
+
+import setuptools
 from setuptools import setup
 
 # The directory containing this file
@@ -7,23 +11,32 @@ HERE = pathlib.Path(__file__).parent
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
+# version
+alphavantage_api_client_version = subprocess.run(['git', 'describe', '--tags'],
+                                                     stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
+assert "." in alphavantage_api_client_version
+assert os.path.isfile("alphavantage_api_client/version.py")
+with open("alphavantage_api_client/VERSION","w", encoding="utf-8") as fh:
+    fh.write(f'{alphavantage_api_client_version}\n')
+
 # This call to setup() does all the work
 setup(
     name="alphavantage-api-client",
-    version="0.0.1",
+    version=alphavantage_api_client_version,
     description="Interact with Alphavantage REST API",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/xrgarcia/alphavantage-api-client",
-    author="Ray Garcia",
-    author_email="ray@slashbin.us",
+    author="Slashbin, LLC",
+    author_email="support@slashbin.us",
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
     ],
-    packages=["alphavantage_api_client"],
+    packages=setuptools.find_packages(),
+    package_data={},
     include_package_data=True,
     install_requires=["requests"],
     entry_points={
