642986efb0c7057092f781d38fac0aca2dba43cf
Ray Garcia <ray@slashbin.us>
2022-06-30 08:49:01 -0500

    added checks for success and raise an error when something fails. this will help end to end tests

--- app.py
+++ app.py
@@ -8,14 +8,34 @@ if __name__ == "__main__":
     }
     result = {}
     client = AlphavantageClient()
+
     company_overview = client.get_company_overview(event)
-    print(company_overview.json())
+    if not company_overview.success:
+        raise ValueError('Unable to get company overview')
+    else:
+        print(company_overview.json())
+
     global_quote = client.get_global_quote(event)
-    print(global_quote.json())
+    if not global_quote.success:
+        raise ValueError("Unable to get global quote")
+    else:
+        print(global_quote.json())
+
     intraday_quote = client.get_intraday_quote(event)
-    print(intraday_quote.json())
+    if not intraday_quote.success:
+        raise ValueError("Unable to get intraday quote")
+    else:
+        print(intraday_quote.json())
+
     earnings = client.get_earnings(event)
-    print(earnings.json())
+    if not earnings.success:
+        raise ValueError("Unable to get earnings")
+    else:
+        print(earnings.json())
+
     cash_flow = client.get_cash_flow(event)
-    print(cash_flow.json())
+    if not cash_flow.success:
+        raise ValueError("Unable to get Cashflow")
+    else:
+        print(cash_flow.json())
     print('------ complete test --------')
